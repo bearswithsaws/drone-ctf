1f0decee1d311f7ebdb9e89bb3f837092877b5f6
weetster <weetster.sec@gmail.com>
2026-07-31 12:27:03 -0400

    Implement entity resource simulation (E2.4, #11)

---
+++ agent/sim/__init__.py
@@ -0,0 +1,17 @@
+"""Local entity simulation."""
+
+from agent.sim.entity_sim import (
+    BuildingSimState,
+    DroneSimState,
+    EntitySim,
+    EntitySimulator,
+    SimulationDelta,
+)
+
+__all__ = [
+    "BuildingSimState",
+    "DroneSimState",
+    "EntitySim",
+    "EntitySimulator",
+    "SimulationDelta",
+]
